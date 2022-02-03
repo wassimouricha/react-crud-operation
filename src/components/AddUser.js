ddf71b5a4ef299095e916580607512d4ad8b8b7d
wassim <wbouricha5@gmail.com>
2022-02-03 09:07:37 +0100

    v5

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import { Link,useHistory } from 'react-router-dom';
 import {  
@@ -10,21 +10,27 @@ import {
 } from "reactstrap";
 
 export const AddUser = () => {
+    const [name, setName]= useState('');
     const {addUser} = useContext(GlobalContext);
     const history = useHistory();
     const onSubmit = () =>{
         const newUser = {
             id: 4,
-            name: 'utilisateur quatre'
+            name
         }
         addUser(newUser);
         history.push('/');
     }
+
+    const onChange = (e) => {
+        setName(e.target.value);
+    }
+ 
   return (
       <Form onSubmit={onSubmit}>
           <FormGroup>
               <Label>Nom</Label>
-              <Input type='text' placeholder='Entrez voter nom'></Input>
+              <Input type='text'  value={name} onChange={onChange} placeholder='Entrez voter nom'></Input>
           </FormGroup>
           <Button type='Submit' >Envoyer</Button>
           <Link to="/" className="btn btn-danger ms-auto">Annuler</Link>
